7f2c3ab5b3a3e4b0132da84f41a6740760d2c6de
Cristian0813 <javier.0813@hotmail.com>
2023-01-18 23:40:42 -0500

    Se soluciona el ejercicio de la ranita

---
+++ script.js
@@ -0,0 +1,107 @@
+let imgchange = document.getElementById("imgchanges");
+
+let btninicio = document.getElementById("inicio");
+let btnlvl1 = document.getElementById("nivel_1");
+let btnlvl2 = document.getElementById("nivel_2");
+let btnlvl3 = document.getElementById("nivel_3");
+let btnlvl4 = document.getElementById("nivel_4");
+let btnlvl5 = document.getElementById("nivel_5");
+let btnlvl6 = document.getElementById("nivel_6");
+let btnlvl7 = document.getElementById("nivel_7");
+let btnlvl8 = document.getElementById("nivel_8");
+let btnlvl9 = document.getElementById("nivel_9");
+let btnlvl10 = document.getElementById("nivel_10");
+let btnlvl11 = document.getElementById("nivel_11");
+let btnlvl12 = document.getElementById("nivel_12");
+let btnlvl13 = document.getElementById("nivel_13");
+let btnlvl14 = document.getElementById("nivel_14");
+let btnlvl15 = document.getElementById("nivel_15");
+let btnlvl16 = document.getElementById("nivel_16");
+let btnlvl17 = document.getElementById("nivel_17");
+let btnlvl18 = document.getElementById("nivel_18");
+let btnlvl19 = document.getElementById("nivel_19");
+let btnlvl20 = document.getElementById("nivel_20");
+let btnlvl21 = document.getElementById("nivel_21");
+let btnlvl22 = document.getElementById("nivel_22");
+let btnlvl23 = document.getElementById("nivel_23");
+let btnlvl24 = document.getElementById("nivel_24");
+let btnfin = document.getElementById("nivel_fin");
+
+btninicio.onclick = function() {
+    imgchange.src = "./image/Comienzo.jpg";
+}
+btnlvl1.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_1.jpg";
+}
+btnlvl2.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_2.jpg";
+}
+btnlvl3.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_3.jpg";
+}
+btnlvl4.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_4.jpg";
+}
+btnlvl5.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_5.jpg";
+}
+btnlvl6.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_6.jpg";
+}
+btnlvl7.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_7.jpg";
+}
+btnlvl8.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_8.jpg";
+}
+btnlvl9.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_9.jpg";
+}
+btnlvl10.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_10.jpg";
+}
+btnlvl11.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_11.jpg";
+}
+btnlvl12.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_12.jpg";
+}
+btnlvl13.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_13.jpg";
+}
+btnlvl14.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_14.jpg";
+}
+btnlvl15.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_15.jpg";
+}
+btnlvl16.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_16.jpg";
+}
+btnlvl17.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_17.jpg";
+}
+btnlvl18.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_18.jpg";
+}
+btnlvl19.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_19.jpg";
+}
+btnlvl20.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_20.jpg";
+}
+btnlvl21.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_21.jpg";
+}
+btnlvl22.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_22.jpg";
+}
+btnlvl23.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_23.jpg";
+}
+btnlvl24.onclick = function() {
+    imgchange.src = "./image/Flexbox_Froggy_nivel_24.jpg";
+}
+btnfin.onclick = function() {
+    imgchange.src = "./image/Todos_los_niveles_completados.jpg";
+}
